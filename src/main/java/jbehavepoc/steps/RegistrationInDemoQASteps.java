d42545f208bf5704d138c0d83922f446e14d8240
liroingo <liroingo@gmail.com>
2017-02-07 17:18:29 -0500

    Registration steps are created

---
+++ src/main/java/jbehavepoc/steps/RegistrationInDemoQASteps.java
@@ -0,0 +1,37 @@
+package jbehavepoc.steps;
+
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Pending;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+
+/**
+ * Created by linfante on 2/7/2017.
+ */
+public class RegistrationInDemoQASteps {
+
+    @Given("I open http://demoqa.com/registration/")
+    @Pending
+    public void givenIOpenHttpdemoqacomregistration() {
+        // PENDING
+    }
+
+    @When("I fill the registration form")
+    @Pending
+    public void whenIFillTheRegistrationForm() {
+        // PENDING
+    }
+
+    @When("I submit the registration form")
+    @Pending
+    public void whenISubmitTheRegistrationForm() {
+        // PENDING
+    }
+
+    @Then("A Thanks message is displayed")
+    @Pending
+    public void thenAThanksMessageIsDisplayed() {
+        // PENDING
+    }
+
+}
---
+++ src/main/resources/jbehavepoc/stories/RegistrationInDemoQA.story
@@ -0,0 +1,12 @@
+Meta:
+
+Narrative:
+As a user
+I want to perform an action
+So that I can achieve a business goal
+
+Scenario: scenario description
+Given I open http://demoqa.com/registration/
+When I fill the registration form
+And I submit the registration form
+Then A Thanks message is displayed
